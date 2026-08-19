2807786ffc98c8f42c29d767cc249c16174d2545
LucasToledo16 <d2022002767@unifei.edu.br>
2026-08-19 13:15:20 -0300

    Refactor SET command parsing and validation
    Replace fixed-offset parsing with token-based argument
    extraction.
    
    Validate SET command arguments using strtol and reject
    invalid values before updating the counter.

--- app/CtrlCoreModule.cpp
+++ app/CtrlCoreModule.cpp
@@ -7,6 +7,11 @@
 #include <cstdlib>
 #include "mbed.h"
 
+#define SET_COMMAND_ID "@SET"
+#define INC_COMMAND_ID "@INC"
+#define DEC_COMMAND_ID "@DEC"
+#define ZER_COMMAND_ID "@ZER"
+
 DigitalOut led_inc(LED1);
 DigitalOut led_dec(LED2);
 DigitalOut led_zer(LED3);
@@ -28,38 +33,45 @@ void CtrlCoreModule::_task() {
   while(true) {
     bool status;
     CtrlIntfModuleMessage *p_ctrl_intf_mod_msg;
+    char *set_value = nullptr;
+    char *saveptr = nullptr;
+    char *endptr = nullptr;
 
     /* Reads messages */
     status = _try_get_for_cb(rtos::Kernel::wait_for_u32_forever,
         &p_ctrl_intf_mod_msg);
     assert(status);
 
-    /* Processes command */
-    if(!strcmp(p_ctrl_intf_mod_msg->buff, "@INC")) {
+    /* Process command */
+    if (strstr(p_ctrl_intf_mod_msg->buff, INC_COMMAND_ID) != nullptr){
       led_inc = 1;
       ThisThread::sleep_for(500ms);
       ++_count;
       led_inc = 0;
-    } else if(!strcmp(p_ctrl_intf_mod_msg->buff, "@DEC")) {
-      if(_count > 0) {
-        led_dec = 1;
-        ThisThread::sleep_for(500ms);
-        --_count;
-        led_dec = 0;
-      }
-    } else if(!strcmp(p_ctrl_intf_mod_msg->buff, "@ZER")) {
-      led_zer = 1;
-      ThisThread::sleep_for(500ms);
-      _count = 0;
-      led_zer = 0;
-    } else if (!strncmp(p_ctrl_intf_mod_msg->buff,"@SET ", 5)){
-        int new_value = strtol(p_ctrl_intf_mod_msg->buff + 5, nullptr, 10);
-        if(new_value >= 0 ){
+    } else if (strstr(p_ctrl_intf_mod_msg->buff, DEC_COMMAND_ID) != nullptr){
+          led_dec = 1;
+          ThisThread::sleep_for(500ms);
+          if(_count > 0){
+            --_count;
+          }
+          led_dec = 0;
+    } else if (strstr(p_ctrl_intf_mod_msg->buff, ZER_COMMAND_ID) != nullptr){
+          led_zer = 1;
+          ThisThread::sleep_for(500ms);
+          _count = 0;
+          led_zer = 0;
+
+
+    } else if (strstr(p_ctrl_intf_mod_msg->buff, SET_COMMAND_ID) != nullptr){
+          set_value = strtok_r(p_ctrl_intf_mod_msg->buff, " \n", &saveptr);
+          set_value = strtok_r(NULL, " \n", &saveptr);
+          int value = strtol(set_value,&endptr,10);
           led_set = 1;
           ThisThread::sleep_for(500ms);
-          _count = new_value;
+          if(set_value != endptr && *endptr == '\0' && value >= 0){
+            _count =  value;
+          }
           led_set = 0;
-        }
     }
 
     /* Writes message response */
